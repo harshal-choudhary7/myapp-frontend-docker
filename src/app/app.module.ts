95534da1226dae28a8b3e5e59bfdc84e8c5de27b
Harshal Choudhary <harshalchoudhary7@gmail.com>
2019-12-03 15:45:09 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProductListService } from './product-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ProductListService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
